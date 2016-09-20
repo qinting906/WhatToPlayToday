34e88f3eba78b71e812fb79c0ded06b0341221e2
qinting513 <513924632@qq.com>
2016-09-20 19:15:39 +0800

    createProject
    createProject

---
+++ WhatToPlayToday/WhatToPlayToday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatToPlayToday
+//
+//  Created by Qinting on 16/9/20.
+//  Copyright © 2016年 Qinting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatToPlayToday/WhatToPlayToday/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatToPlayToday
+//
+//  Created by Qinting on 16/9/20.
+//  Copyright © 2016年 Qinting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
